bc43eaa3e6715cad2d80e2eeac8d58e2539253d3
Thanos Atskakanis <thanosatskakanis@gmail.com>
2016-04-29 14:16:22 +0100

    error when adding duplicate product

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,25 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    @@products.each do |product| 
+      if title == product.title
+        raise DuplicateProductError, "#{title} already exists."
+      end
+    end
+    @@products << self
   end
 end

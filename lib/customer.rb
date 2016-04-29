992e73c4ad067f3e39b90bfcdf4067c59f2efc21
Thanos Atskakanis <thanosatskakanis@gmail.com>
2016-04-29 15:18:33 +0100

    Customer class, methods and error

---
+++ lib/customer.rb
@@ -0,0 +1,34 @@
+class Customer
+  attr_reader :name
+
+  @@customers = []
+
+  def initialize(options={})
+    @name = options[:name]
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+  # looks for a customer with the passed name. returns nil if nothing found
+  def self.find_by_name(name)
+    @@customers.each do |customer| 
+      return customer if name == customer.name
+    end
+    nil
+  end
+
+  private
+
+  def add_to_customers
+    customer = Customer.find_by_name(@name)
+    if ! customer.nil?
+      raise DuplicateCustomerError, "#{name} already exists."
+    end
+    @@customers << self
+  end
+
+
+end
